4b79797f124a6df19f68d8f5d453bfac669dd635
Jean Pierre La Russa <jplarussa@gmail.com>
2022-09-22 04:48:17 -0300

    Agrego busqueda de cotizacion e informe de cryptomonedas ganadoras y perdedoras del dia. Ajustes finales

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // Creo Array de cryptos
 const tokens= [];
 
+// Creo la clase constructora de Cryptos
 class crypto{
     constructor(name,ticker,price,change,lastDay){
         this.name = name;
@@ -11,6 +12,7 @@ class crypto{
     }
 }
 
+// Creo instancias y las meto al array
 let btc = new crypto("Bitcoin","BTC","18674",0.018,2)
 let eth = new crypto("Ethereum","ETH","1260",0.058,2)
 let bnb = new crypto("BNB","BNB","1260",0.008,2)
@@ -25,23 +27,44 @@ tokens.push(xrp);
 tokens.push(ada);
 tokens.push(sol);
 
-console.log(tokens);
+// Ingreso del usuario y filtrado
+let input = prompt("Indique la cryptomoneda para conocer su cotización:").toUpperCase();
+let output = tokens.find((item) => item.ticker === input);
+// Hago mas amigable la información de si subió o bajo la crypto seleccionada
+if (output.lastDay === 1) {
+    output.lastDay = "Subió";
+} else if (output.lastDay === 2) {
+    output.lastDay = "Bajo";
+} else {
+    output.lastDay = "Neutro";
+};
+// Muestro el resultado
+let showToken = `
+    Name: ${output.name}
+    Ticker: ${output.ticker}
+    Price: ${output.price}
+    Change: ${output.change*100}%
+    Last Day: ${output.lastDay}
+`;
+alert(showToken);
 
-const winners = tokens.filter(crypto => crypto.lastDay === 1);
-winners.forEach(item => {
-    let mostrar = `Cryptos Ganadoras del día: \n ${item.ticker}`;
-    alert(mostrar);
-    console.log(mostrar);
-});
-
-const losers = tokens.filter(crypto => crypto.lastDay === 2);
-losers.forEach(item => {
-    let mostrar = `Cryptos Perdedoras del día: \n ${item.ticker}`;
-    alert(mostrar);
-    console.log(mostrar)
-});
-
-
+// Funcion del botón web, resumen de cotizaciones
 function cotizaciones () {
+    const winners = tokens.filter(crypto => crypto.lastDay === 1);
+    let showWinners = `Cryptos Ganadoras del día: \n`;
+    winners.forEach(item => {
+        showWinners += `${item.ticker} `;
+    });
+    alert(showWinners);
+    console.log(showWinners);
+
+    const losers = tokens.filter(crypto => crypto.lastDay === 2);
+    let showLosers = `Cryptos Perdedoras del día: \n`;
+    losers.forEach(item => {
+        showLosers += `${item.ticker} `;
+    });
+    alert(showLosers);
+    console.log(showLosers);
     
+    console.log(tokens);
 }
