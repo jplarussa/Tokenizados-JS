e77689c0271195f04acd092fd817b23ef8abec5b
Jean Pierre La Russa <jplarussa@gmail.com>
2022-10-06 23:57:21 -0300

    Ajustes Preentrega 2

--- js/contacto.js
+++ js/contacto.js
@@ -6,6 +6,7 @@ if (cryptosStorage) {
     cryptos = cryptosStorage;
 }
 
+const domUsuario = document.getElementById("forminscrip");
 const domSelector = document.getElementById("cryptocliente");
 
 // Renderizar las opciones select de cada crypto 
@@ -14,10 +15,36 @@ function renderizarCryptos() {
         let option = document.createElement("option");
         option.setAttribute("value", `"${item.name}"`);
         option.innerHTML = `${item.name}`;
-        
+
         // Lo insertamos
         domSelector.append(option);
     })
 }
 
 renderizarCryptos();
+
+let usuario;
+let usuarioStorage = sessionStorage.getItem("usuario");
+if(usuarioStorage){
+    usuario = usuarioStorage;
+    let mensaje = `Bienvenido ${usuario}`;
+    domSelector.textContent(mensaje);
+}
+
+let nombrecliente = document.getElementById("nombrecliente");
+let apellidocliente = document.getElementById("apellidocliente");
+let emailcliente = document.getElementById("emailcliente");
+let cryptocliente = document.getElementById("cryptocliente");
+
+let formulario = document.getElementById("forminscrip-fc");
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let inputs = e.target.children;
+    console.log(inputs[0].value);
+    console.log(inputs[1].value);
+    if(!inputs[0].value.includes(".gmail")){
+    inputs[0].value = "";
+    }
+})
