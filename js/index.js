b86175ec02cb0909b93e04c0dbced7fdef6e1c23
Jean Pierre La Russa <jplarussa@gmail.com>
2022-10-21 04:07:35 -0300

    Ajustes de diseño, cambio por axios

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 // API Coingecko con 15 cryptos de mayor MarketCap
-apigecko = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false"
+apigecko = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
 
 // Declaro constantes que toman elementos del DOM
 const domTitulo = document.getElementById("titulo-contacto");
 
 // Creo Array de cryptos
-const tokens = [];
+let tokens = [];
 let registrado;
 
 // Chequeo cliente en sessionStorage y muestro bienvenida si existe.
@@ -40,42 +40,27 @@ class crypto {
 
 const getTokens = async (url) => {    
     try {
-        let allTokens = [];
-        let response = await fetch(url);
-        data = await response.json();
-        allTokens.push(...data);
-        return allTokens;
+        let response = await axios(url);
+        let data = await response.data;
+        tokens = data;
+        localStorage.setItem("cryptos", JSON.stringify(tokens));
     } catch (error) {
         console.log(error);
     }
 };
 
+//Llamo a la función para traer datos de Coingecko
+getTokens(apigecko);
 
 
-// Creo instancias y las meto al array
-let btc = new crypto("Bitcoin", "BTC", 18674, 0.018, 2)
-let eth = new crypto("Ethereum", "ETH", 1260.50, 0.058, 2)
-let bnb = new crypto("BNB", "BNB", 274, 0.008, 2)
-let xrp = new crypto("XRP", "XRP", 0.486, 0.024, 1)
-let ada = new crypto("Cardano", "ADA", 0.44, 0.022, 2)
-let sol = new crypto("Solana", "SOL", 32.2, 0.024, 2)
-
-tokens.push(btc);
-tokens.push(eth);
-tokens.push(bnb);
-tokens.push(xrp);
-tokens.push(ada);
-tokens.push(sol);
-
-// Cargo en LocalStorage el listado de cryptos
-localStorage.setItem("cryptos", JSON.stringify(tokens));
 
 const domMonedas = document.getElementById("monedas");
 const domBoton = document.getElementById("boton");
 // Muestro cotizaciones al clickear
 domBoton.addEventListener("click", renderizarTokens)
-let coco = getTokens(apigecko);
-console.log(coco);
+
+
+
 
 
 // Renderizar las cotizaciones en tarjetas
@@ -92,30 +77,37 @@ function renderizarTokens() {
         nodoCardTitle.classList.add("card-header", "py-3");
         const nodoTokenTitle = document.createElement("h4");
         nodoTokenTitle.classList.add("my-0", "fw-normal");
-        nodoTokenTitle.textContent = item.symbol;
+        nodoTokenTitle.textContent = item.symbol.toUpperCase();
         const nodoCardBody = document.createElement("div");
         nodoCardBody.classList.add("card-body");
         const nodoCardPrice = document.createElement("H1");
         nodoCardPrice.classList.add("card-title", "pricing-card-title");
-        nodoCardPrice.textContent = `${item.currentPrice}`;
+        nodoCardPrice.textContent = `${item.current_price}`;
         const nodoCardText = document.createElement("ul");
         nodoCardText.classList.add("list-unstyled", "mt-3", "mb-4");
-        nodoCardText.innerHTML = `<li>${item.name}</li>`
+        nodoCardText.innerHTML = `
+        <li>${item.name}</li>
+        <li>${Math.round(item.price_change_percentage_24h * 100)/100}%</li>
+        `
         // Cambio el color segun variacion de la cotizacion
-        switch (item.lastDay) {
-            case 1:
+        switch (true) {
+            case item.price_change_percentage_24h > 0:
                 nodoCard.classList.add("border-success");
                 nodoCardTitle.classList.add("bg-success", "border-success");
                 nodoCardPrice.classList.add("text-success");
                 nodoCardText.classList.add("text-success");
                 break;
-            case 2:
+            case item.price_change_percentage_24h < 0:
                 nodoCard.classList.add("border-danger");
                 nodoCardTitle.classList.add("bg-danger", "border-danger");
                 nodoCardPrice.classList.add("text-danger");
                 nodoCardText.classList.add("text-danger");
                 break;
             default:
+                nodoCard.classList.add("border-secondary");
+                nodoCardTitle.classList.add("bg-secondary", "border-secondary");
+                nodoCardPrice.classList.add("text-secondary");
+                nodoCardText.classList.add("text-secondary");
                 break;
         }
         // Lo insertamos
